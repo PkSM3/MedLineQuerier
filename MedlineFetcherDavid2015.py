e42d4a07dedf79c336d8d606e87cd443a9f0088a
PkSM3 <pokesam3@gmail.com>
2015-01-22 14:45:00 +0100

    Global Limit OK -> MedLine/*.xml

--- MedlineFetcherDavid2015.py
+++ MedlineFetcherDavid2015.py
@@ -26,15 +26,13 @@ personalpath = PersonalPath('MedLine/')
 personalpath.createDirectory()
 
 
-
-
 # Return the:
 # - count = 
 # - queryKey = 
 # - webEnv = 
 def medlineEsearch(query):
 
-    print ("MedlineFetcher::medlineFetcher :")
+    print ("MedlineFetcher::medlineEsearch :")
 
     "Get number of results for query 'query' in variable 'count'"
     "Get also 'queryKey' and 'webEnv', which are used by function 'medlineEfetch'"
@@ -61,7 +59,8 @@ def medlineEsearch(query):
     # queryKey = doc.xpathEval('eSearchResult/QueryKey/text()')[0]
     # webEnv = doc.xpathEval('eSearchResult/WebEnv/text()')[0]
     # print count, queryKey, webEnv
-    return str(count), queryKey, webEnv
+    values = { "count": int(str(count)), "queryKey": queryKey , "webEnv":webEnv }
+    return values
 
 def medlineEfetch(query, retmax):
     
@@ -102,67 +101,102 @@ def medlineEfetch(query, retmax):
     print (count , ' results written to file ' , pubMedResultFileName , '\n' )
 
 
-
-
-def medlineEfetchRAW(query, retmax , limit):
+# RETMAX:
+# Total number of UIDs from the retrieved set to be shown in the XML output (default=20)
+# maximum of 100,000 records
+def medlineEfetchRAW( fullquery):
     
-    print ("MedlineFetcher::medlineEfetch :")
+
+    query = fullquery["string"]
+    retmax = fullquery["retmax"]
+    count = fullquery["count"]
+    queryKey = fullquery["queryKey"]
+    webEnv = fullquery["webEnv"]
+
+    print ("MedlineFetcher::medlineEfetchRAW :")
 
     "Fetch medline result for query 'query', saving results to file every 'retmax' articles"
 
     queryNoSpace = query.replace(' ', '') # No space in directory and file names, avoids stupid errors
     
 
-    pubmedqueryfolder = personalpath.pubMedAbstractsPath + 'Pubmed_' + queryNoSpace
-    if not os.path.isdir(pubmedqueryfolder):
-        os.makedirs(pubmedqueryfolder)
+    # pubmedqueryfolder = personalpath.pubMedAbstractsPath + 'Pubmed_' + queryNoSpace
+    # if not os.path.isdir(pubmedqueryfolder):
+    #     os.makedirs(pubmedqueryfolder)
 
-    pubMedResultFileName = pubmedqueryfolder + '/Pubmed_' + queryNoSpace + '.xml'
+    pubMedResultFileName = personalpath.mainPath + 'Pubmed_' + queryNoSpace + '.xml'
     pubMedResultFile = open(pubMedResultFileName, 'w')
     
-    count1, queryKey, webEnv = medlineEsearch(query)
 
-    print ('Submitted query ' , query , ' gives ' , count1 , ' results')
-    print ('Starting fetching at ' , time.asctime(time.localtime()) , '\n')
-    
-   #  Fetch results...
+    print ('Query "' , query , '"\t:\t' , count , ' results')
+    print ('Starting fetching at ' , time.asctime(time.localtime()) )
 
-
-    count = int(count1)
-    count = limit  
     retstart = 0
-
     while(retstart < count):
-        print (str(retstart) )
         eFetch = '%s/efetch.fcgi?email=youremail@example.org&rettype=%s&retmode=xml&retstart=%s&retmax=%s&db=%s&query_key=%s&WebEnv=%s' %(pubMedEutilsURL, reportType, retstart, retmax, pubMedDB, queryKey, webEnv)                
         eFetchResult = urlopen(eFetch)
         if sys.version_info >= (3, 0): pubMedResultFile.write(eFetchResult.read().decode('utf-8'))
         else: pubMedResultFile.write(eFetchResult.read())
         retstart += retmax
-        # if retstart>limit: break
+        break # you shall not pass !!
 
     pubMedResultFile.close()
     print ('Fetching for query ' , query , ' finished at ' , time.asctime(time.localtime()) )
-    print (count , ' results written to file ' , pubMedResultFileName , '\n' )
+    print (retmax , ' results written to file ' , pubMedResultFileName , '\n' )
+    print("------------------------------------------")
 
 
 
 
 
 
-def serialFetcher(yearsNumber,query):
+# GLOBALLIMIT:
+# I will retrieve this exact amount of publications.
+# The publications per year i'll retrieve per year will be = (k/N)*GlobalLimit <- i'll use this as RETMAX
+# - k : Number of publications of x year (according to pubmed)
+# - N : Sum of every k belonging to {X} (total number of pubs according to pubmed)
+# - GlobalLimit : Number of publications i want.
+def serialFetcher(yearsNumber,query, globalLimit):
+
+    N = 0
+
     print ("MedlineFetcher::serialFetcher :")
+    thequeries = []
     for i in range(yearsNumber):
-        year = str(2015 - i)
+        year = str(2014 - i)
         print ('YEAR ' + year)
         print ('---------\n')
         # medlineEfetch(str(year) + '[dp] '+query , 20000)
-        medlineEfetchRAW(str(year) + '[dp] '+query , retmax=20000, limit=100)
+        # medlineEfetchRAW(str(year) + '[dp] '+query , retmax=300)
+        pubmedquery = str(year) + '[dp] '+query
+        globalresults = medlineEsearch(pubmedquery)
+        N+=globalresults["count"]
+        querymetadata = { 
+            "string": pubmedquery , 
+            "count": globalresults["count"] , 
+            "queryKey":globalresults["queryKey"] , 
+            "webEnv":globalresults["webEnv"] , 
+            "retmax":0 
+        }
+        thequeries.append ( querymetadata )
+
+    print("Total Number:", N,"publications")
+    print("And i want just:",globalLimit,"publications")
+    print("---------------------------------------\n")
+
+    for query in thequeries:
+        k = query["count"]
+        percentage = k/float(N)
+        retmax_forthisyear = int(round(globalLimit*percentage))
+        query["retmax"] = retmax_forthisyear
+        # print(query)
+        medlineEfetchRAW( query )
+
     print ('Done !')
 # regler les parametres de serialFetcher:
 
 
-
-serialFetcher(1, 'microbiota')
+# serialFetcher(yearsNumber=3, 'microbiota' , globalLimit=100 )
+serialFetcher( 3, 'microbiota' , 300 )
 # query = str(2015)+ '[dp] '+'microbiota'
 # medlineEsearch( query )
